e1c387aa9505e0682ae85a858a0cb9f0dec0dab2
austinxduong <axduong2@gmail.com>
2021-06-20 18:23:10 -0700

    first test setup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,16 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Order from '../lib/models/Order.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+  it('it posts a new order and sends a SMS text', async () => {
+    const res = request(app)
+      .post('/api/v1/orders')
+      .send({ quantity: 3 });
+    expect(res.body).toEqual({ id: '1', quantity: 3 });
   });
 });
